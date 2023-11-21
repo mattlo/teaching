c34f9cbe5eea6d0b1374b3a2654b9ba67fa45bb0
vuhungthang <vuhungthang2707@gmail.com>
2023-11-20 23:57:43 -0600

    typeTest submission

--- exercises/basics/types.ts
+++ exercises/basics/types.ts
@@ -1,3 +1,15 @@
 export function returnAllTypesObject() : {[key : string] : any} {
   // @TODO implement
+  return {
+    typeString: "This is a string",
+    typeNumber: 77,
+    typeBoolean: true,
+    typeArray: [27,77,87],
+    typeObject: {myFavNum : 27},
+    typeFunction: function () {},
+    typeUndefined: undefined,
+    typeNull: null,
+    typeRegularExpression: /thangvu/
+  }
+
 }
