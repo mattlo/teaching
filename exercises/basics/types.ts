9ea4c57ffa4c4735d7e674308e1259c80050fa8c
vuhungthang <111513635+vuhungthang@users.noreply.github.com>
2023-11-25 01:41:02 -0600

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 39243c670e5763525f75bc5b31807f03bcca8bbe, reversing
    changes made to 60d264ab92955534c9d9ed79f0d2ff8b8d705ac5.

--- exercises/basics/types.ts
+++ exercises/basics/types.ts
@@ -1,14 +1,3 @@
 export function returnAllTypesObject() : {[key : string] : any} {
-  return {
-    typeString: "This is a string",
-    typeNumber: 77,
-    typeBoolean: true,
-    typeArray: [27,77,87],
-    typeObject: {myFavNum : 27},
-    typeFunction: function () {},
-    typeUndefined: undefined,
-    typeNull: null,
-    typeRegularExpression: /thangvu/
-  }
-
+  // @TODO implement
 }
