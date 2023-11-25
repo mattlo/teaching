b814b6364a17ff4d368d76044b901d24fd60e85c
vuhungthang <111513635+vuhungthang@users.noreply.github.com>
2023-11-25 12:53:50 -0600

    Update functions.ts
    updated questions 3 and 4

--- exercises/basics/functions.ts
+++ exercises/basics/functions.ts
@@ -5,7 +5,7 @@ export function returnFunctionalDeclarationNamedBob() : any {
     return function bob() {
     }
 }
-    export function returnObjectWithMethodNamedBobThatReturnsFunctionalDeclarationNamedBob() : any {
+export function returnObjectWithMethodNamedBobThatReturnsFunctionalDeclarationNamedBob() : any {
     return {
         bob: function() {
             return function bob() {
@@ -14,31 +14,20 @@ export function returnFunctionalDeclarationNamedBob() : any {
     }
 }
 export function curryConcept1(input : number, callback : (input2 : number) => number) {
-    //return callback(3*2)+input // This one is cheating, however, it still passes, LMAO
-    const result = function(input2:number) {
-        return callback(input2) + input*2;
-    }
-    return result(input)
-// This one is cheating,too but I will ask you later!
+    return callback(input) + (input * 2);
 }
 export function returnNestedFunction10LevelsDeep() {
-    return function(lv1:1) {
-        return function(lv2:2){
-            return function(lv3:3) {
-                return function(lv4:4){
-                    return function (lv5:5){
-                        return function(lv6:6) {
-                            return function(lv7:7) {
-                                return function(lv8:8){
-                                    return function(lv9:9) {
-                                        return lv1+lv2+lv3+lv4+lv5+lv6+lv7+lv8+lv9;
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
+    const nestedFunctions = (lv1: number) =>
+        (lv2: number) =>
+            (lv3: number) =>
+                (lv4: number) =>
+                    (lv5: number) =>
+                        (lv6: number) =>
+                            (lv7: number) =>
+                                (lv8: number) =>
+                                    (lv9: number) =>
+                                        (lv10:number) =>
+                                            lv1 + lv2 + lv3 + lv4 + lv5 + lv6 + lv7 + lv8 + lv9+ lv10;
+    const result = nestedFunctions(0)
+    return result;
 }
