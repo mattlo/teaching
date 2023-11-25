76ca4f96e98000492aa6affb2fbb6decd9ed0800
vuhungthang <111513635+vuhungthang@users.noreply.github.com>
2023-11-25 01:44:29 -0600

    Revert "fill in questions in functions.ts"
    This reverts commit 5d90cf1b41d075338bf481f0b9f1ef541711369e.

--- exercises/basics/functions.ts
+++ exercises/basics/functions.ts
@@ -1,49 +1,25 @@
 export function returnFunctionalExpression() : any {
-    return function() {}
+  // @TODO implement (aka: "impl")
 }
 
 export function returnFunctionalDeclarationNamedBob() : any {
-    return function bob() {
-
-    }
+  // @TODO impl
+  //       `bob` should be the name
 }
-    export function returnObjectWithMethodNamedBobThatReturnsFunctionalDeclarationNamedBob() : any {
-    return {
-        bob: function() {
-            return function bob() {
 
-            }
-        }
-    }
+export function returnObjectWithMethodNamedBobThatReturnsFunctionalDeclarationNamedBob() : any {
+  // @TODO impl
+  //       `bob` should be the name
 }
 
 export function curryConcept1(input : number, callback : (input2 : number) => number) {
-    //return callback(3*2)+input // This one is cheating, however, it still passes, LMAO
-    const result = function(input2:number) {
-        return callback(input2) + input*2;
-    }
-    return result(input)
-// This one is cheating,too but I will ask you later!
+  // @TODO impl
+  //       you must combine `input` and `callback`
 }
-export function returnNestedFunction10LevelsDeep() {
-    return function(lv1:1) {
-        return function(lv2:2){
-            return function(lv3:3) {
-                return function(lv4:4){
-                    return function (lv5:5){
-                        return function(lv6:6) {
-                            return function(lv7:7) {
-                                return function(lv8:8){
-                                    return function(lv9:9) {
-                                        return lv1+lv2+lv3+lv4+lv5+lv6+lv7+lv8+lv9;
-                                    }
-                                }
-                            }
-                        }
-                    }
 
-                }
-            }
-        }
-    }
+export function returnNestedFunction10LevelsDeep() {
+  // @TODO impl
+  // @TODO in typescript, you'll need to set the parameter type as `number`
+  //       otherwise the test will complain
+  //       (hello : number) => {}
 }
