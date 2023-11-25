5d90cf1b41d075338bf481f0b9f1ef541711369e
vuhungthang <111513635+vuhungthang@users.noreply.github.com>
2023-11-25 01:25:27 -0600

    fill in questions in functions.ts

--- exercises/basics/functions.ts
+++ exercises/basics/functions.ts
@@ -1,25 +1,49 @@
 export function returnFunctionalExpression() : any {
-  // @TODO implement (aka: "impl")
+    return function() {}
 }
 
 export function returnFunctionalDeclarationNamedBob() : any {
-  // @TODO impl
-  //       `bob` should be the name
-}
+    return function bob() {
 
-export function returnObjectWithMethodNamedBobThatReturnsFunctionalDeclarationNamedBob() : any {
-  // @TODO impl
-  //       `bob` should be the name
+    }
+}
+    export function returnObjectWithMethodNamedBobThatReturnsFunctionalDeclarationNamedBob() : any {
+    return {
+        bob: function() {
+            return function bob() {
+
+            }
+        }
+    }
 }
 
 export function curryConcept1(input : number, callback : (input2 : number) => number) {
-  // @TODO impl
-  //       you must combine `input` and `callback`
+    //return callback(3*2)+input // This one is cheating, however, it still passes, LMAO
+    const result = function(input2:number) {
+        return callback(input2) + input*2;
+    }
+    return result(input)
+// This one is cheating,too but I will ask you later!
 }
-
 export function returnNestedFunction10LevelsDeep() {
-  // @TODO impl
-  // @TODO in typescript, you'll need to set the parameter type as `number`
-  //       otherwise the test will complain
-  //       (hello : number) => {}
+    return function(lv1:1) {
+        return function(lv2:2){
+            return function(lv3:3) {
+                return function(lv4:4){
+                    return function (lv5:5){
+                        return function(lv6:6) {
+                            return function(lv7:7) {
+                                return function(lv8:8){
+                                    return function(lv9:9) {
+                                        return lv1+lv2+lv3+lv4+lv5+lv6+lv7+lv8+lv9;
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                }
+            }
+        }
+    }
 }
